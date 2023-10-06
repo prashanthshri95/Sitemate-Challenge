35225a5e3585d6bb9af612a04b4ac4e5299be595
prashanthshri95 <47488846+prashanthshri95@users.noreply.github.com>
2023-10-06 13:09:31 +1100

    Create simple Rest APIs for server operations

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 // app.js
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+let issue = {
+    id: 1,
+    title: 'Sample Issue',
+    description: 'This is a sample issue for demonstration purposes.'
+};
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// Create Operation
+app.post('/api/issues', (req, res) => {
+    const newIssue = req.body;
+    console.log('Created Issue:', newIssue);
+    res.json({ message: 'Issue created successfully.' });
+});
+
+// Read Operation
+app.get('/api/issues', (req, res) => {
+    console.log('Read Issue:', issue);
+    res.json(issue);
+});
+
+// Update Operation
+app.put('/api/issues', (req, res) => {
+    const updatedIssue = req.body;
+    console.log('Updated Issue:', updatedIssue);
+    res.json({ message: 'Issue updated successfully.' });
+});
+
+// Delete Operation
+app.delete('/api/issues/:id', (req, res) => {
+    const issueId = req.params.id;
+    console.log('Deleted Issue ID:', issueId);
+    res.json({ message: 'Issue deleted successfully.' });
+});
+
 
 const port = process.env.PORT || 8082;
 
